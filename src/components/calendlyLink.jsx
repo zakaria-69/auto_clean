91271bca2449c4ec512d451f218d9d1f4e3680df
unknown <ladjrafi_zakaria@yahoo.com>
2024-12-11 20:04:31 +0100

    created a component for rendering dynamic month on calendly

---
+++ src/components/calendlyLink.jsx
@@ -0,0 +1,18 @@
+import React , {useState, useEffect} from "react";
+
+const CalendlyLink = () => {
+    const [calendlyLink, setCalendyLink] = useState("");
+
+    useEffect(() => {
+        const currentMonth = new Date().toISOString().slice(0,7);
+        setCalendyLink(`https://calendly.com/ladjrafi-zak/30min?month=${currentMonth}`);
+    },[])
+
+    return(
+        <a href={calendlyLink} target="_blank" rel="noopener noreferrer">
+      Prendre rendez-vous
+    </a>
+    )
+}
+
+export default CalendlyLink;
