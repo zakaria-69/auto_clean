d21bd03fbffe030317e61ea165ae48ad08e3144c
unknown <ladjrafi_zakaria@yahoo.com>
2024-12-11 20:09:51 +0100

    indented component

--- src/components/calendlyLink.jsx
+++ src/components/calendlyLink.jsx
@@ -1,18 +1,20 @@
-import React , {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 const CalendlyLink = () => {
-    const [calendlyLink, setCalendyLink] = useState("");
+  const [calendlyLink, setCalendyLink] = useState("");
 
-    useEffect(() => {
-        const currentMonth = new Date().toISOString().slice(0,7);
-        setCalendyLink(`https://calendly.com/ladjrafi-zak/30min?month=${currentMonth}`);
-    },[])
+  useEffect(() => {
+    const currentMonth = new Date().toISOString().slice(0, 7);
+    setCalendyLink(
+      `https://calendly.com/ladjrafi-zak/30min?month=${currentMonth}`
+    );
+  }, []);
 
-    return(
-        <a href={calendlyLink} target="_blank" rel="noopener noreferrer">
+  return (
+    <a href={calendlyLink} target="_blank" rel="noopener noreferrer">
       Prendre rendez-vous
     </a>
-    )
-}
+  );
+};
 
 export default CalendlyLink;
