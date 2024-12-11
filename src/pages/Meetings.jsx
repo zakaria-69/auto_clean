3247673ebaa188f297dca802c66c43df5e9d2788
unknown <ladjrafi_zakaria@yahoo.com>
2024-12-11 20:08:30 +0100

    created meetings page

--- src/pages/Meetings.jsx
+++ src/pages/Meetings.jsx
@@ -1,9 +1,34 @@
+import Footer from "../components/footer";
+import Header from "../components/Header";
+import CalendlyLink from "../components/calendlyLink";
+
 const Meetings = () => {
-    return (
-        <section className="meetings_container">
-                inside meetings
-        </section>
-    )
-}
+  return (
+    <section className="meetings_container">
+      <Header />
+      <div className="meetings_container_subcontainer">
+        <div className="meetings_container_subcontainer_titleAndText">
+          <h1>Rendez-vous</h1>
+          <p>
+            Vous souhaitez prendre rendez-vous pour une de nos formules ou un de
+            nos services ?
+          </p>
+          <span>C&apos;est ici que sa ce passe : </span>
+        </div>
+        <div className="meetings_container_subcontainer_card_container meetings_card_one">
+          <div className="meetings_container_subcontainer_card_container_card">
+            <h2>Lyon</h2>
+            <p>Lyon 69000</p>
+            <CalendlyLink />
+          </div>
+          <div className="meetings_container_subcontainer_operator">
+            <p>Chez zakaria</p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </section>
+  );
+};
 
 export default Meetings;
