221ddabdd76db139c41e3858d6ab9e03b1107d7c
unknown <ladjrafi_zakaria@yahoo.com>
2024-12-11 15:44:12 +0100

    created a Header component to re use it easely in other pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import logo from "../../src/assets/img/logo_auto_clean.avif"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faSnapchat,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+<header className="home_container_header">
+        <div className="home_container_header_logo">
+          <a href="/">
+            <img src={logo} alt="logo de voiture qui brille" />
+          </a>
+        </div>
+        <div className="home_container_header_left_nav">
+          <nav>
+            <ul>
+              <li>
+                <a href="" className="navLink header_accueil">
+                  Accueil
+                </a>
+              </li>
+              <li>
+                <a href="" className="navLink header_meetings">
+                  Rendez-vous
+                </a>
+              </li>
+              <li>
+                <a href="" className="navLink header_contact">
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a href="" className="navLink header_city">
+                  Lyon
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="home_container_header_social_medias">
+          <nav>
+            <ul>
+              <li>
+                <a href="" className="navLink header_accueil">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+              </li>
+              <li>
+                <a href="" className="navLink header_meetings">
+                  <FontAwesomeIcon icon={faSnapchat} />
+                </a>
+              </li>
+              <li>
+                <a href="" className="navLink header_contact">
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </li>
+              <li>
+                <a href="" className="navLink header_city">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+}
+export default Header;
