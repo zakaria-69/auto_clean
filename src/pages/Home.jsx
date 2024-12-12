cf9e5fc6c561192a4070660dda7b7d5859ec40e2
unknown <ladjrafi_zakaria@yahoo.com>
2024-12-12 17:51:58 +0100

    corrected capital F for footer import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Decouverte from "../components/Home/Decouverte";
 import Description from "../components/Home/Description";
