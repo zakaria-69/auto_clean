84af077ebee292a7888aab7f3cf0f6aff6c7fe12
unknown <ladjrafi_zakaria@yahoo.com>
2025-01-05 17:54:08 +0100

    adapted informations page for light-theme

--- src/pages/Informations.jsx
+++ src/pages/Informations.jsx
@@ -3,8 +3,11 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faPhone, faHome } from "@fortawesome/free-solid-svg-icons";
+import { useContext } from "react";
+import { ThemeContext } from "../components/theme";
 
 const Informations = () => {
+  const {theme} = useContext(ThemeContext);
   return (
     <section className="information_container">
       <Header />
@@ -17,7 +20,7 @@ const Informations = () => {
           <div className="information_container_subcontainer_infosContainer_contact">
             <ul>
               <li className="information_container_subcontainer_infosContainer_contact_adress">
-              <FontAwesomeIcon icon={faHome} className="icon" />
+              <FontAwesomeIcon icon={faHome} color={theme.icon} className="icon" />
                 <a
                   href="https://maps.app.goo.gl/Qu5WrAk7HbJ9dWxz5"
                   target="_blank"
@@ -26,13 +29,13 @@ const Informations = () => {
                 </a>
               </li>
               <li className="information_container_subcontainer_infosContainer_contact_phone">
-              <FontAwesomeIcon icon={faPhone} className="icon" />
+              <FontAwesomeIcon icon={faPhone} color={theme.icon} className="icon" />
                 <a href="tel:+33782479774" target="_blank">
                   +33 7 82 47 97 74
                 </a>
               </li>
               <li className="information_container_subcontainer_infosContainer_contact_email">
-              <FontAwesomeIcon icon={faEnvelope} className="icon" />
+              <FontAwesomeIcon icon={faEnvelope} color={theme.icon} className="icon" />
                 <a href="mailto:ladjrafi_zakaria@yahoo.com" target="_blank">
                   {" "}
                   ladjrafi_zakaria@yahoo.com
@@ -92,9 +95,9 @@ const Informations = () => {
         <div className="information_appointment_subcontainer">
           <h2>Prendre rendez-vous</h2>
           <p>dans cet atelier</p>
-          <a href="" target="_blank">
+          {/* <a href="" target="_blank"> */}
             <CalendlyLink />
-          </a>
+          {/* </a> */}
         </div>
       </section>
       <div className="about_me">
@@ -102,7 +105,7 @@ const Informations = () => {
             Ce site est un site vitrine réalisé dans le cadre d&apos;un projet personnel.
             Je suis Zakaria Ladjrafi développeur fullstack vous pouvez découvrir mes réseau via les liens présent dans les menus.
         </p>
-        <p>Merci</p>
+        <p>Merci !</p>
       </div>
       <Footer />
     </section>
