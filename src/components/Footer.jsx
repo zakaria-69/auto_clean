7a0c05d718089d4752b6d59796e3c753f39380a1
unknown <ladjrafi_zakaria@yahoo.com>
2025-01-19 12:44:42 +0100

    adapted footer map for responsive design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -103,10 +103,11 @@ const Footer = () => {
         <iframe
           // eslint-disable-next-line max-len
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44538.52695156109!2d4.793930673298822!3d45.75800318058034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ea516ae88797%3A0x408ab2ae4bb21f0!2sLyon!5e0!3m2!1sfr!2sfr!4v1733871758989!5m2!1sfr!2sfr"
-          style={{ border: 0 }}
+          // style={{ border: 0 }}
           allowFullScreen
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
+          id="footer_map"
         />
       </div>
     </footer>
