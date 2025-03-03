28501a17260d2f946c0fe374d3ea0f9b872b4243
unknown <ladjrafi_zakaria@yahoo.com>
2025-03-03 18:17:47 +0100

    cleaned App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,1 @@
-import { useState } from 'react'
 
-
-// function App() {
-//   // const [count, setCount] = useState(0)
-
-//   return (
-    
-//       <div>
-//         <p>page main</p>
-//         </div>
-
-  
-//   )
-// }
-
-// export default App;
